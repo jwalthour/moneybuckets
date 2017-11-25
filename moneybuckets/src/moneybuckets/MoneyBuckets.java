8d5f9e5c1cfe3758e0fb26e3e65a365af6d9b470
John Walthour <john.walthour@gmail.com>
2017-11-25 17:58:01 -0500

    Initial eclipse project

---
+++ moneybuckets/src/moneybuckets/MoneyBuckets.java
@@ -0,0 +1,10 @@
+package moneybuckets;
+
+public class MoneyBuckets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
